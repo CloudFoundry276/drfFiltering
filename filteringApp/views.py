d70c7fa81372c88c38d3f97d0b3b3c1a0fa8d3ce
CloudFoundry276 <cloudfoundry276@gmail.com>
2022-03-15 09:25:14 +0530

    django rest framework filtering done

--- filteringApp/views.py
+++ filteringApp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from filteringApp.models import Student
+from filteringApp.serializers import StudentSerializer
+from rest_framework import viewsets, filters
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 # Create your views here.
+class StudentPagination(PageNumberPagination):
+    page_size = 1
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    pagination_class = LimitOffsetPagination
+    # filter_backends = [DjangoFilterBackend]
+    # filterset_fields = ['name', 'exam_score']
+    # filter_backends = [filters.SearchFilter]
+    # search_fields = ['id', 'name']
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['name', 'exam_score']
+    ordering = ['id']
